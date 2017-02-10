f25fa706832da3e853511f2ed88a115754da2336
ZJQian <2326274522@qq.com>
2017-02-10 17:45:59 +0800

    viewController

--- QRCodeDemo/ViewController.m
+++ QRCodeDemo/ViewController.m
@@ -8,15 +8,59 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property  (nonatomic, strong)UITableView *tableView;
+@property  (nonatomic, strong)NSMutableArray *viewControllers;
 
 @end
 
 @implementation ViewController
 
+- (UITableView *)tableView{
+    
+    if (!_tableView) {
+        _tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) style:UITableViewStylePlain];
+        _tableView.delegate = self;
+        _tableView.dataSource = self;
+    }
+    return _tableView;
+}
+- (NSMutableArray *)viewControllers {
+
+    if (!_viewControllers) {
+        _viewControllers = [NSMutableArray arrayWithArray:@[@"ScanQRViewController",
+                                                            @"MakeQRCodeViewController",
+                                                            @"MakeBarCodeViewController"]];
+    }
+    return _viewControllers;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self.view addSubview:self.tableView];
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    return self.viewControllers.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    static NSString *cellID = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+    if (!cell) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellID];
+    }
+    cell.textLabel.text = self.viewControllers[indexPath.row];
+    return cell;
+}
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    Class myClass = NSClassFromString(self.viewControllers[indexPath.row]);
+    [self.navigationController pushViewController:[[myClass alloc]init] animated:YES];
 }
 
 - (void)didReceiveMemoryWarning {
