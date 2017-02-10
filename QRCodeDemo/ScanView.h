95814959e6e759cb1469998656f96ef05dfca6d2
ZJQian <2326274522@qq.com>
2017-02-10 17:45:44 +0800

    自定义view

---
+++ QRCodeDemo/ScanView.h
@@ -0,0 +1,26 @@
+//
+//  ScanView.h
+//  QRCodeDemo
+//
+//  Created by ZJQ on 2017/2/10.
+//  Copyright © 2017年 ZJQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScanView : UIView
+
+
+/**
+ 开始动画
+ */
+- (void)run;
+
+
+/**
+ 停止动画
+ */
+- (void)pause;
+
+
+@end
